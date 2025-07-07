d181288ae7c6e8e115e53a5428d15210af297bc8
willowsenator <willowsenator@gmail.com>
2025-07-07 22:25:25 +0200

    verify merkle proof for airdrop

--- src/BagelToken.sol
+++ src/BagelToken.sol
@@ -5,8 +5,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BagelToken is ERC20, Ownable {
-    constructor() ERC20("Bagel Token", "BAGEL") Ownable(msg.sender) {
-    }
+    constructor() ERC20("Bagel Token", "BAGEL") Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,5 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
 contract MerkleAirdrop {
+    using SafeERC20 for IERC20;
+
+    error MerkleAirdrop__InvalidProof();
+
+    address[] claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+
+    event Claim(address indexed account, uint256 amount);
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        // Initialize the contract with the Merkle root
+        // This is where you would store the Merkle root for verification
+
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        // Calculate the leaf node from the account and amount
+        // This is the hash of the account and amount, which will be used to verify against
+        bytes32 leaf = keccak256(bytes.concat(abi.encode(account, amount)));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+    }
 }
